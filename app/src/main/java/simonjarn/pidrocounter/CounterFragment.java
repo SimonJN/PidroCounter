a774f1bbff7d81d7ab53b1bec25ff4900ccd8e20
Simon Järn <simon.jarn@gmail.com>
2019-09-21 14:34:59 +0300

    Add main content Add the main content frame and the different fragments. - Add Settings, history and main counter fragments - Add Main activity with navigation bar at the bottom - Set up the main project structures - Add a custom shadow for the navigation and title bars - Add a title bar with text

---
+++ app/src/main/java/simonjarn/pidrocounter/CounterFragment.java
@@ -0,0 +1,31 @@
+package simonjarn.pidrocounter;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CounterFragment extends Fragment {
+
+
+    public CounterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.counter_fragment, container, false);
+    }
+
+}
---
+++ app/src/main/java/simonjarn/pidrocounter/MainActivity.java
@@ -0,0 +1,51 @@
+package simonjarn.pidrocounter;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+    public Fragment counter_fragment = new CounterFragment();
+    public Fragment history_fragment = new HistoryFragment();
+    public Fragment settings_fragment = new SettingsFragment();
+
+    public FragmentManager fm = getSupportFragmentManager();
+
+    public BottomNavigationView main_nav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        main_nav = (BottomNavigationView) findViewById(R.id.main_navigation);
+        //Set the middle button as selected
+        main_nav.setSelectedItemId(R.id.action_counter);
+        //Load the first fragment
+        fm.beginTransaction().replace(R.id.main_container, counter_fragment).addToBackStack(null).commit();
+        //Set the click listener for the navigation
+        main_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_history:
+                        fm.beginTransaction().replace(R.id.main_container, history_fragment).addToBackStack(null).commit();
+                        return true;
+                    case R.id.action_counter:
+                        fm.beginTransaction().replace(R.id.main_container, counter_fragment).addToBackStack(null).commit();
+                        return true;
+                    case R.id.action_settings:
+                        fm.beginTransaction().replace(R.id.main_container, settings_fragment).addToBackStack(null).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
