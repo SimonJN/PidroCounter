2ac873d1c9231dc6cb310f7d87143d61bb3524c3
SimonJN <simon.jarn@gmail.com>
2024-01-09 21:50:28 +0200

    Update to API34 - Update the target API to level 34 - Add link to privacy policy in the app - Add the privacy policy - Change name of the settings page to the info page and reorder som stuff

--- app/src/main/java/simonjarn/pidrocounter/SettingsFragment.java
+++ app/src/main/java/simonjarn/pidrocounter/SettingsFragment.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -25,7 +27,11 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.settings_fragment, container, false);
+        View settings_fragment = inflater.inflate(R.layout.settings_fragment, container, false);
+        // Set the privacy policy link to be clickable
+        TextView privacy_policy_text = settings_fragment.findViewById(R.id.privacy_policy);
+        privacy_policy_text.setMovementMethod(LinkMovementMethod.getInstance());
+        return settings_fragment;
     }
 
 }
